9307638c93ce4c692dcd409bb6fcb6654918c98f
17214290001 <17214290001@OLAB04-D18.academico.iesb.br>
2019-09-24 20:37:33 -0300

    Atividade10

---
+++ Atividade10/src/java/managedbeans/LoginMB.java
@@ -0,0 +1,17 @@
+
+package managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean
+@RequestScoped
+public class LoginMB {
+
+    
+    public LoginMB() {
+        
+    }
+    
+}
